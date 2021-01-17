8c8479163c275637e9cac61c7e0b15119519afac
Rohan Garg <rohangarg@Rohans-MacBook-Pro.local>
2021-01-17 15:58:52 +0530

    minor changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    redirect_to posts_url if @post.user_id != current_user&.id
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
